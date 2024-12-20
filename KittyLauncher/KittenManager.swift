8c24e8a6ae269ed673ff881ab4b4831c0bd6e2d6
Yasser Bal <yasserbal@icloud.com>
2024-12-21 00:17:32 +0100

    KittenManager

---
+++ KittyLauncher/KittenManager.swift
@@ -0,0 +1,38 @@
+//
+//  KittenManager.swift
+//  KittyLauncher
+//
+//  Created by Yasser Bal on 21/12/2024.
+//
+
+import Foundation
+
+class KittenManager: ObservableObject {
+    @Published var kittens: Set<Kitten> = []
+
+    public func newKitty() {
+        do {
+            let k = Kitten()
+            k.terminationHandler =  { _ in
+                self.kittens = self.kittens.filter({ $0.isRunning })
+            }
+            try k.run()
+            self.kittens.insert(k)
+        } catch {
+            // FIXME: Handle Errors
+            fatalError("Failed to run kitten : \(error)")
+        }
+    }
+
+    public func kill(kitten: Kitten) {
+        kitten.terminate()
+        self.kittens.remove(kitten)
+    }
+    
+    public func quitAll() {
+        kittens.forEach { kitten in
+            kitten.terminate()
+            self.kittens.remove(kitten)
+        }
+    }
+}
