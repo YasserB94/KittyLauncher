2f4cc91f15de5d33effdf14538899b2814d89587
Yasser Bal <yasserbal@icloud.com>
2024-12-20 23:59:41 +0100

    Initial Commit

---
+++ KittyLauncher/KittyLauncherApp.swift
@@ -0,0 +1,17 @@
+//
+//  KittyLauncherApp.swift
+//  KittyLauncher
+//
+//  Created by Yasser Bal on 20/12/2024.
+//
+
+import SwiftUI
+
+@main
+struct KittyLauncherApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
