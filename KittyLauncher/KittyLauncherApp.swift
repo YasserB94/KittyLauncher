2210d3450e82bbedd863f61446d51bcbd13c1660
Yasser Bal <yasserbal@icloud.com>
2024-12-21 00:56:03 +0100

    View

--- KittyLauncher/KittyLauncherApp.swift
+++ KittyLauncher/KittyLauncherApp.swift
@@ -9,9 +9,38 @@ import SwiftUI
 
 @main
 struct KittyLauncherApp: App {
+    @StateObject private var manager = KittenManager()
+    
     var body: some Scene {
-        WindowGroup {
-            Text("Hi Mom")
+        MenuBarExtra {
+            Button("🦁 New Kitty", systemImage: "plus.rectangle",action:self.manager.newKitty)
+            Divider()
+            ForEach(Array(manager.kittens),id:\.self) { kitten in
+                Menu("\(kitten.emoji) \(kitten.pid)"){
+                    Button(action: {
+                        manager.kill(kitten: kitten)
+                    }, label: {
+                        Text("💀 Kill ")
+                    })
+                }
+            }
+            Divider()
+            if(manager.kittens.count > 0) {
+                Button("🪦 Killall ",systemImage: "xmark.circle.fill"){
+                    manager.quitAll()
+                }
+            }else {
+                Text("🥺 No kittens ")
+                Button("Quit", systemImage: "xmark.circle.fill") {
+                    NSApplication.shared.terminate(nil)
+                }
+            }
+        } label: {
+            Image("kitty")
+                .resizable()
+                .scaledToFit()
+                .frame(height:20)
         }
     }
+
 }
